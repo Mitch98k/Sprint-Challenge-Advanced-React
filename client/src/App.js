d755bf8cdd45b06d2eca7baa7a731379ae18d99a
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2020-01-17 15:15:37 -0700

    Created the useLocalStorage custom hook.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import {FetchData} from './components/fetch';
+import {useLocalStorage} from './hooks/useLocalStorage';
 import './App.css';
 
 function App() {
   return (
+        <div>
           <FetchData /> 
+          <useLocalStorage />
+        </div>
   );
 }
 
